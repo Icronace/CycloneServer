d33781996bef6e7bbb238278ccc4ed63edfbf72e
server <icronace@gmail.com>
2016-12-07 15:03:01 +0100

    first commit

---
+++ UnityPlugin/Packet.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace De.Cyclone.Network
+{
+    public interface Packet
+    {
+        void Read(PacketBuffer buffer);
+
+        void Write(PacketBuffer buffer);
+    }
+
+    public struct HandshakePacket : Packet
+    {
+        public String Host;
+        public ushort Port;
+        public bool Reconnect;
+
+        public HandshakePacket(String host, ushort port, bool reconnect)
+        {
+            Host = host;
+            Port = port;
+            Reconnect = reconnect;
+        }
+
+        public void Read(PacketBuffer buffer)
+        {
+            Host = buffer.ReadString();
+            Port = buffer.ReadUnsignedShort();
+        }
+
+        public void Write(PacketBuffer buffer)
+        {
+            buffer.WriteString(Host);
+            buffer.WriteUnsignedShort(Port);
+        }
+    }
+
+    public struct EncryptionRequestPacket : Packet
+    {
+        // Modulus, Exponent, Signature
+        public byte[] Challenge;
+
+        public EncryptionRequestPacket(byte[] challenge)
+        {
+            Challenge = challenge;
+        }
+
+        public void Read(PacketBuffer buffer)
+        {
+            Challenge = new byte[buffer.ReadUnsignedInt()];
+            buffer.Read(Challenge, 0, Challenge.Length);
+        }
+
+        public void Write(PacketBuffer buffer)
+        {
+            buffer.Write(Challenge, 0, Challenge.Length);
+        }
+    }
+
+    public struct EncryptionResponsePacket : Packet
+    {
+        public byte[] SharedSecret;
+        public byte[] Challenge;
+
+        public EncryptionResponsePacket(byte[] sharedSecret, byte[] challenge)
+        {
+            SharedSecret = sharedSecret;
+            Challenge = challenge;
+        }
+
+        public void Read(PacketBuffer buffer)
+        {
+            SharedSecret = new byte[buffer.ReadUnsignedInt()];
+            buffer.Read(SharedSecret, 0, SharedSecret.Length);
+            Challenge = new byte[buffer.ReadUnsignedInt()];
+            buffer.Read(Challenge, 0, Challenge.Length);
+        }
+
+        public void Write(PacketBuffer buffer)
+        {
+            buffer.Write(SharedSecret, 0, SharedSecret.Length);
+            buffer.Write(Challenge, 0, Challenge.Length);
+        }
+    }
+
+    public struct AuthenticationPacket : Packet
+    {
+        public void Read(PacketBuffer buffer)
+        {
+
+        }
+
+        public void Write(PacketBuffer buffer)
+        {
+
+        }
+    }
+}
---
+++ UnityPlugin/PacketBuffer.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace De.Cyclone.Network
+{
+    public partial class PacketBuffer
+    {
+        private static Encoding encoding = Encoding.UTF8;
+
+        public byte ReadUnsignedByte()
+        {
+            int value = stream.ReadByte();
+            if (value == -1) {
+                throw new EndOfStreamException();
+            }
+            return (byte) value;
+        }
+
+        public void WriteUnsignedByte(byte value)
+        {
+            stream.WriteByte(value);
+        }
+
+        public new sbyte ReadByte()
+        {
+            return (sbyte) ReadUnsignedByte();
+        }
+
+        public void WriteByte(sbyte value)
+        {
+            WriteUnsignedByte((byte) value);
+        }
+
+        public ushort ReadUnsignedShort()
+        {
+            return (ushort) ((ReadUnsignedByte() << 8) | ReadUnsignedByte());
+        }
+
+        public void WriteUnsignedShort(ushort value)
+        {
+            Write(new byte[] { (byte) ((value >> 8) & 0xFF), (byte) (value & 0xFF) }, 0, 2);
+        }
+
+        public short ReadShort()
+        {
+            return (short) ReadUnsignedShort();
+        }
+
+        public void WriteShort(short value)
+        {
+            WriteUnsignedShort((ushort) value);
+        }
+
+        public uint ReadUnsignedInt()
+        {
+            return (uint) ((ReadUnsignedByte() << 24) | (ReadUnsignedByte() << 16) | (ReadUnsignedByte() << 8) | ReadUnsignedByte());
+        }
+
+        public void WriteUnsignedInt(uint value)
+        {
+            Write(new byte[] { (byte) ((value >> 24) & 0xFF), (byte) ((value >> 16) & 0xFF), (byte) ((value >> 8) & 0xFF), (byte) (value & 0xFF) }, 0, 4);
+        }
+
+        public int ReadInt()
+        {
+            return (int) ReadUnsignedInt();
+        }
+
+        public void WriteInt(int value)
+        {
+            WriteUnsignedInt((uint) value);
+        }
+
+        public ulong ReadUnsignedLong()
+        {
+            return (ulong) ((ReadUnsignedByte() << 56) | (ReadUnsignedByte() << 48) | (ReadUnsignedByte() << 40) | (ReadUnsignedByte() << 32) | (ReadUnsignedByte() << 24) | (ReadUnsignedByte() << 16) | (ReadUnsignedByte() << 8) | ReadUnsignedByte());
+        }
+
+        public void WriteUnsignedLong(ulong value)
+        {
+            Write(new byte[] { (byte) ((value >> 56) & 0xFF), (byte) ((value >> 48) & 0xFF), (byte) ((value >> 40) & 0xFF), (byte) ((value >> 32) & 0xFF), (byte) ((value >> 24) & 0xFF), (byte) ((value >> 16) & 0xFF), (byte) ((value >> 8) & 0xFF), (byte) (value & 0xFF) }, 0, 4);
+        }
+
+        public long ReadLong()
+        {
+            return (long) ReadUnsignedLong();
+        }
+
+        public void WriteLong(long value)
+        {
+            WriteUnsignedLong((ulong) value);
+        }
+
+        public unsafe float ReadFloat()
+        {
+            uint value = ReadUnsignedInt();
+            return *(float*)&value;
+        }
+
+        public unsafe void WriteFloat(float value)
+        {
+            WriteUnsignedInt(*(uint*)&value);
+        }
+
+        public unsafe double ReadDouble()
+        {
+            ulong value = ReadUnsignedLong();
+            return *(double*)&value;
+        }
+
+        public unsafe void WriteDouble(double value)
+        {
+            WriteUnsignedLong(*(ulong*)&value);
+        }
+
+        public string ReadString()
+        {
+            var data = new byte[ReadInt()];
+            Read(data, 0, data.Length);
+            return encoding.GetString(data);
+        }
+
+        public void WriteString(string value)
+        {
+            WriteInt(encoding.GetByteCount(value));
+            Write(encoding.GetBytes(value), 0, encoding.GetByteCount(value));
+        }
+    }
+}
